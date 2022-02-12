41dd7454aa60807d9392f88b9bb3fa9fa33c566a
Git-hub-Eduard <81467036+Git-hub-Eduard@users.noreply.github.com>
2022-02-12 18:40:10 +0200

    Добавлено считывание данных с XML файла

---
+++ Prospector Solitaire/Assets/__Scripts/Layout.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Globalization;
+
+
+[System.Serializable]
+public class SlotDef
+{
+    public float x;
+    public float y;
+    public bool faceUp = false;
+    public string layerName = "Default";
+    public int layerID = 0;
+    public int id;
+    public List<int> hiddenBy = new List<int>();
+    public string type = "slot";
+    public Vector2 stagger;
+}
+
+public class Layout : MonoBehaviour
+{
+    public PT_XMLReader xmlr;
+    public PT_XMLHashtable xml;
+    public Vector2 multiplier;//Смещение од центра раскладки
+    //Ссылка SlotDef
+    public List<SlotDef> slotDefs;
+    public SlotDef drawPile;
+    public SlotDef discardPile;
+    //Хранит имена всех рядов
+    public string[] SortingLayerNames = new string[] { "Row0", "Row1", "Row2", "Row3", "Discard", "Draw" };
+    
+    //Эта функция вызываетса для чтения файла LayoutXML.xml
+    public void ReadLayout(string xmlText)
+    {
+        xmlr = new PT_XMLReader();
+        xmlr.Parse(xmlText);// Загрузить xml
+        xml = xmlr.xml["xml"][0];//Загрузить xml
+        //Прочитать множители, определяющие растояние между картами
+        multiplier.x = float.Parse(xml["multiplier"][0].att("x"), CultureInfo.InvariantCulture);
+        multiplier.y = float.Parse(xml["multiplier"][0].att("y"), CultureInfo.InvariantCulture);
+        //Прочитать слоты
+        SlotDef tSD;
+        //slotsX используетса для ускорения доступа к элементам <slot>
+        PT_XMLHashList slotsX = xml["slot"];
+        for(int i = 0;i<slotsX.Count;i++)
+        {
+            tSD = new SlotDef();
+            if(slotsX[i].HasAtt("type"))
+            {
+                // Если <slot> имеет атрибут type, прочитать его
+                tSD.type = slotsX[i].att("type");
+            }
+            else
+            {
+                // Иначе определить тип как "slot"; это отдельная карта в ряду
+                tSD.type = "slot";
+            }
+            //Преобразовать атрибуты в числовые значения 
+            tSD.x = float.Parse(slotsX[i].att("x"), CultureInfo.InvariantCulture);
+            tSD.y = float.Parse (slotsX[i].att("y"), CultureInfo.InvariantCulture);
+            tSD.layerID = int.Parse(slotsX[i].att("layer"));
+            //Преобразовать номер ряда  в текст LayerName
+            tSD.layerName = SortingLayerNames[tSD.layerID];
+            switch (tSD.type)
+            {
+                //Прочитать дополнительные атрибуты, опираясь на тип слота
+                case "slot":
+                    tSD.faceUp = (slotsX[i].att("faceup") == "1");
+                    tSD.id = int.Parse (slotsX[i].att("id"));
+                    if(slotsX[i].HasAtt("hiddenby"))
+                    {
+                        string[] hiding = slotsX[i].att("hiddenby").Split(',');
+                        foreach(string s in hiding)
+                        {
+                            tSD.hiddenBy.Add(int.Parse(s));
+                        }
+                    }
+                    slotDefs.Add(tSD);
+                    break;
+                case "drawpile":
+                    tSD.stagger.x = float.Parse(slotsX[i].att("xstagger"), CultureInfo.InvariantCulture);
+                    drawPile = tSD;
+                    break;
+                case "discardpile":
+                    discardPile = tSD;
+                    break;
+            }
+        }
+    }
+
+}
--- Prospector Solitaire/Assets/__Scripts/Prospector.cs
+++ Prospector Solitaire/Assets/__Scripts/Prospector.cs
@@ -9,8 +9,10 @@ public class Prospector : MonoBehaviour
     static public Prospector S;
     [Header("Set in Inspector")]
     public TextAsset deckXML;
+    public TextAsset layoutXML;
     [Header("Set Dynamically")]
     public Deck deck;
+    public Layout layout;
     // Start is called before the first frame update
     void Awake()
     {
@@ -22,16 +24,15 @@ public class Prospector : MonoBehaviour
         deck = GetComponent<Deck>();// Получить компонент  Deck
         deck.InitDeck(deckXML.text);// Передать ему DeckXML
         Deck.Shuffle(ref deck.cards);//Перемешать колоду карт
-        Cart c;
-        for(int cNum = 0; cNum<deck.cards.Count; cNum++)
-        {
-            c = deck.cards[cNum];
-            c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
-        }
+       // Cart c;
+       // for(int cNum = 0; cNum<deck.cards.Count; cNum++)
+        //{
+        //    c = deck.cards[cNum];
+         //   c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+        //}
+        layout = GetComponent<Layout>();//Получить компонент Layout
+        layout.ReadLayout(layoutXML.text);
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+   
 }
